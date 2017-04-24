afe997f60921380be28c12fba184241b9fd33937
Andreas Loeffler <aloeff@de.ibm.com>
2017-04-24 14:05:09 +0200

    grid gallery + carousel image. clicking grid image does not yet show correct image in carousel

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ view images from directory via bootstrap carousel #node.js, #express
 
 based on sample code by pradeep330:
 http://bootsnipp.com/snippets/featured/thumbnail-carousel-single-image-sliding
+
+or
+http://bootsnipp.com/snippets/featured/carousel-with-gallery-thumbs
--- public/javascripts/images.js
+++ public/javascripts/images.js
@@ -18,4 +18,22 @@ $(document).ready(function () {
     //         $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
     //     }
     // });
+  //Handles the carousel thumbnails
+  $('[id^=carousel-selector-]').click(function () {
+        var id_selector = $(this).attr("id");
+        try {
+            var id = /-(\d+)$/.exec(id_selector)[1];
+            console.log(id_selector, id);
+            jQuery('#myCarousel').carousel(parseInt(id));
+        } catch (e) {
+            console.log('Regex failed!', e);
+        }
+    });
+        // When the carousel slides, auto update the text
+        $('#myCarousel').on('slid.bs.carousel', function (e) {
+                 var id = $('.item.active').data('slide-number');
+                $('#carousel-text').html($('#slide-content-'+id).html());
+        });
+
+
 });
