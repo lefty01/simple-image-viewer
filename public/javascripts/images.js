682123e1f5b50781f707dd8910963ab138380d87
Andreas Loeffler <aloeff@de.ibm.com>
2017-04-24 13:35:33 +0200

    slide only single image

--- public/javascripts/images.js
+++ public/javascripts/images.js
@@ -1,20 +1,21 @@
 
 $(document).ready(function () {
     $('#myCarousel').carousel({
-        interval: 10000
+        interval: 0
     })
-    $('.fdi-Carousel .item').each(function () {
-        var next = $(this).next();
-        if (!next.length) {
-            next = $(this).siblings(':first');
-        }
-        next.children(':first-child').clone().appendTo($(this));
 
-        if (next.next().length > 0) {
-            next.next().children(':first-child').clone().appendTo($(this));
-        }
-        else {
-            $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-        }
-    });
+    // $('.fdi-Carousel .item').each(function () {
+    //     var next = $(this).next();
+    //     if (!next.length) {
+    //         next = $(this).siblings(':first');
+    //     }
+    //     next.children(':first-child').clone().appendTo($(this));
+
+    //     if (next.next().length > 0) {
+    //         next.next().children(':first-child').clone().appendTo($(this));
+    //     }
+    //     else {
+    //         $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+    //     }
+    // });
 });
